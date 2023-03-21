9248cfc5f473189abd20e3d5dc0113fda95f9560
Jean Sandes <sandesjean.sandes@gmail.com>
2023-03-21 18:33:50 -0300

    Update WebSecurityConfig.java

--- speedyDrive/src/main/java/com/sandes/speedyDrive/configs/security/WebSecurityConfig.java
+++ speedyDrive/src/main/java/com/sandes/speedyDrive/configs/security/WebSecurityConfig.java
@@ -1,5 +1,8 @@
 package com.sandes.speedyDrive.configs.security;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration /* serve pra dizer que é uma classe de configuração*/
 public class WebSecurityConfig {
 
 }
