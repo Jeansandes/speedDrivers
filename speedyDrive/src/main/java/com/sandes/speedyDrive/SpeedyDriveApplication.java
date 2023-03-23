ef6ad9beab909fc948bfab20355a92103f37c41f
Jean Sandes <sandesjean.sandes@gmail.com>
2023-03-23 00:46:52 -0300

    Update SpeedyDriveApplication.java

--- speedyDrive/src/main/java/com/sandes/speedyDrive/SpeedyDriveApplication.java
+++ speedyDrive/src/main/java/com/sandes/speedyDrive/SpeedyDriveApplication.java
@@ -22,7 +22,26 @@ public class SpeedyDriveApplication {
 		SpringApplication.run(SpeedyDriveApplication.class, args);
 	}
 
-	
+	/*@Override
+	public void run(String... args) throws Exception {
+		ClientModel cl1 = new ClientModel(null,"Sandro Bjj","06636274352","cohabiano x");
+		ClientModel cl2 = new ClientModel(null,"Jean Bjj","111111111111","anil 2");
+		
+		CarModel cr1 = new CarModel(null,"X5","BMW",cl1);
+		CarModel cr2 = new CarModel(null,"gol","FIAT",cl2);
+		CarModel cr3 = new CarModel(null,"COROLLA","TOYOTA",cl1);
+		CarModel cr4 = new CarModel(null,"A4","AUID",cl2);
+		
+		cl1.getCars().addAll(Arrays.asList(cr1,cr3));
+		cl2.getCars().addAll(Arrays.asList(cr2,cr4));
+		
+		clientRepository.save(cl1);
+		clientRepository.save(cl2);
+		
+		carRepository.save(cr1);
+		carRepository.save(cr2);
+		carRepository.save(cr3);
+		carRepository.save(cr4);*/
 
 
 		
